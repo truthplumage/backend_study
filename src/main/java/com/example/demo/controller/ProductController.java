342055fdeb00d08a0d300fd478ba497406bae1a2
truthwing <truthplumage@gmail.com>
2026-06-01 00:38:52 +0900

    feat : 서비스에 있는 부분 controller에서 동작되도록 수정.

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,14 +1,47 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ProductCreateRequest;
+import com.example.demo.dto.ProductUpdateRequest;
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("${api.init}/product")
+@RequiredArgsConstructor
 public class ProductController {
-    @GetMapping("/")
-    public String hello(){
-        return "hello world";
+    private final ProductService productService;
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody ProductCreateRequest request) {
+        Product response = productService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{productId}")
+    public Product getById(@PathVariable UUID productId) {
+        return productService.getById(productId);
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @PutMapping("/{productId}")
+    public Product update(@PathVariable UUID productId,
+                          @RequestBody ProductUpdateRequest request) {
+        return productService.update(productId, request);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> delete(@PathVariable UUID productId) {
+        productService.delete(productId);
+        return ResponseEntity.noContent().build();
     }
 }
