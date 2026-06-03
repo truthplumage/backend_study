f87c081fe86217cee45790ddb7911183c8749b8b
truthwing <truthplumage@gmail.com>
2026-06-03 18:52:25 +0900

    feat: 결제 완료 이벤트로 Order 결제 상태 변경

--- src/main/java/com/example/demo/payment/infrastructure/event/PaymentIntegrationEventHandler.java
+++ src/main/java/com/example/demo/payment/infrastructure/event/PaymentIntegrationEventHandler.java
@@ -1,6 +1,9 @@
 package com.example.demo.payment.infrastructure.event;
 
+import com.example.demo.order.application.dto.MarkOrderPaidCommand;
+import com.example.demo.order.application.usecase.OrderUseCase;
 import com.example.demo.payment.application.event.PaymentConfirmedEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -8,10 +11,19 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class PaymentIntegrationEventHandler {
+
+    private final OrderUseCase orderUseCase;
+
     //결제가 완료되었을때 이벤트를 받는 부분. 여기서 오더에 상태를 변경하는 usecase 함수를 실행해주면 됨.
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(PaymentConfirmedEvent event) {
+        orderUseCase.markPaid(new MarkOrderPaidCommand(
+                event.orderId(),
+                event.approvedAt(),
+                event.paymentId()
+        ));
         log.info("Payment confirmed event handled after commit. paymentId={}, orderId={}, approvedAt={}",
                 event.paymentId(),
                 event.orderId(),
