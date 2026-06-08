1013c5c6a9957a6cfff5c851992f861ca46783e9
truthwing <truthplumage@gmail.com>
2026-06-08 12:27:47 +0900

    feat: cors 학습용 소스 추가.

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -10,6 +10,10 @@ import org.springframework.security.config.annotation.web.configurers.FormLoginC
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +21,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
         return httpSecurity.httpBasic(HttpBasicConfigurer::disable)
+                //cors 학습용 소스
+//                .cors(configure->{
+//                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+//                    corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));//허용된 URL
+//                    corsConfiguration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"));// 허용된 메소드
+//                    corsConfiguration.setAllowCredentials(true);
+//                    corsConfiguration.setAllowedHeaders(Arrays.asList("*")); // 허용된 header
+//                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//                    source.registerCorsConfiguration("/api/v1/**", corsConfiguration);//configuration을 적용할 주소 패턴
+//                    configure.configurationSource(source);
+//                })
                 .formLogin(FormLoginConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->
